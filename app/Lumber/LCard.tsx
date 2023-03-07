27dba0ac1f17a958c2b7eb4d0a52fed7c1ce4bb8
Shakeb Siddiqui <sms8508@psu.edu>
2023-03-07 02:20:29 -0500

    first push

---
+++ app/Lumber/LCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Lcard.css";
+
+
+interface CardProps {
+  title: string;
+  description: string;
+  onClick?: () => void; // Optional onClick function
+}
+
+
+const Card: React.FC<CardProps> = ({ title, description, onClick }) => {
+  const handleClick = () => {
+    console.log(`Clicked ${title}`);
+    if (onClick) {
+      onClick();
+    }
+  };
+  return (
+    <div className="card" onClick={onClick}>
+      <h2 className='font'>{title}</h2>
+      <p className='font'>{description}</p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ app/Lumber/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../globals.css";
+import Card from './LCard';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="body">
+      <h1 className = "header-font" > LumberJack</h1>
+      <div className='card-container'>
+      <Card title ="advice 1" description="This is the first card."/>
+      <Card title ="advice 2" description="This is the first card." />
+      <Card title ="advice 3" description="This is the first card." />
+    </div>
+    </div>
+  );
+};
+
+export default App;
