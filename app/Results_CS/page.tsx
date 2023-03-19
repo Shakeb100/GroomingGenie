fb4ad329eb8801f67229e98dedea6a157c1a0e52
Shakeb Siddiqui <sms8508@psu.edu>
2023-03-19 17:47:36 -0400

    page

--- app/Results_CS/page.tsx
+++ app/Results_CS/page.tsx
@@ -12,19 +12,29 @@ const App: React.FC = () => {
     return (
       <div className="body">
         <div className={jose.className}>
-        <h1 className = "header-font" >What you need for a fresh face</h1>
+        <h1 className = "header-font" >First Time Beard</h1>
+
         <div className='white-box'>
-          <div className="text-container">
-            <p>This is some text that will appear to the left of the iframe</p>
+            <div className="text-container">
+              <div className='info-font'>
+              <p className='infohead'>Advice to get you started</p>
+              <p> Be patient: Growing a beard takes time, and it may take several weeks before you see any significant growth. Don't give up too soon and give your beard time to grow.</p>
+              <p>Shape it up: As your beard grows, consider trimming and shaping it to keep it looking neat and well-groomed. You can do this yourself or go to a barber or stylist for professional help.</p>
+              <p>Beard Growth Oil: One way to help speed up the process is to apply a good qulity <a href='https://thebeardclub.com/products/beard-growth-oil' >beard oil</a> that will promote growth and healthy hair. Look below for some good ones!</p>
+              <p>Embrace the itch: When your beard is first growing, you may experience some itchiness. This is normal and will subside as your beard grows longer. Using beard oils and moisturizers well help reduce this. Check out this face moisturizer from <a href="https://www.harrys.com/en/us/products/daily-face-lotion">HARRY'S</a>!</p>
+            </div>
+            </div>
+
+
+
+            <div className='iframe-container'> 
+                <iframe
+                src="https://shakeb100-groominggenie-ai.hf.space"
+                width="770"
+                height="500"
+              ></iframe>
           </div>
-          <div className='center'> 
-        <iframe
-        src="https://shakeb100-stable-diffusion-inpainting.hf.space"
-        frameBorder="0"
-        width="800"
-        height= "565"
-      ></iframe>
-        </div>
+          
         </div>
         </div>
   
