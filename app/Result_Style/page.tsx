9904558ecdfc04affb7968a1ffbdc109b7e91d47
Shakeb Siddiqui <sms8508@psu.edu>
2023-03-21 12:43:41 -0400

    testing reboot

--- app/Result_Style/page.tsx
+++ app/Result_Style/page.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
             <ul>🪒 There are hundreds of posisble styles you can try out on your lucious mane, always be open to experiment</ul>
             <ul>🪒 Different beards look better on different face shapes. Find your face shape and figure out what works best for you!</ul>
             <ul>🪒 Ask you local barbershop for what might work best for you, one on one professional advice always works best!</ul>
-            <ul>🪒 Don't use any old comb lying around your house. Your beard deserves better. Look for a <a href = "https://evanalexandergrooming.com/products/wooden-beard-comb-red-sandalwood?variant=12125823533146&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&gclid=CjwKCAjw5dqgBhBNEiwA7PryaNHGEBh1Z1ITJodf1VU6vxnkGrWtnrOEzZucQXxD70v3_xYM_ZrkFxoCzoAQAvD_BwE">Comb</a>, with teeth as wide as a lumberjack's smile.</ul>
+            <ul>🪒 Don't use any old comb lying around your house. Your beard deserves better, ;pok for a <a href = "https://evanalexandergrooming.com/products/wooden-beard-comb-red-sandalwood?variant=12125823533146&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&gclid=CjwKCAjw5dqgBhBNEiwA7PryaNHGEBh1Z1ITJodf1VU6vxnkGrWtnrOEzZucQXxD70v3_xYM_ZrkFxoCzoAQAvD_BwE">Comb</a>, with teeth as wide as a lumberjack's smile.</ul>
           </div>
           <div>
           <h2 className='title-contain'>Upload a photo of yourself on the right and mask the area around your mouth. Submit to see the magic! Make sure to mask in the style that you want!</h2>
