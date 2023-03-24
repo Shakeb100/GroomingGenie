1f6a2e661aa89b3e1807c0187ccc1843066eb116
Shakeb Siddiqui <sms8508@psu.edu>
2023-03-23 22:15:03 -0400

    done

--- app/Results_CS/page.tsx
+++ app/Results_CS/page.tsx
@@ -14,8 +14,8 @@ const App: React.FC = () => {
       <div className={jose.className}>
       <h1 className = "header-font" >Fresh Face</h1>
 
-      <div className='white-box'>
-          <div className="clean-container">
+      <div className="freshface-white-box">
+      <div className="freshface-clean-container">
             <div className='info-font'>
             <p className='infohead'>Advice for that baby face</p>
             <ul> 🪒A good quality razor is essential. Choose a razor that works best for your skin type. <a href = "https://www.harrys.com/en/us/products/the-truman">HARRY'S Razors</a> are designed with precision-engineered blades and a flexible hinge for a comfortable, close shave.</ul>
@@ -23,11 +23,10 @@ const App: React.FC = () => {
             <ul>🪒 Make sure to lather up with shaving cream or you might end up looking like you just dipped your face in a vat of hot lava. Trust us, it's not a good look. Use HARRY'S <a href='https://www.harrys.com/en/us/products/shave-cream'> Shave Cream</a> to make sure that doesn't happen.</ul>
             <ul>🪒 Just like Batman needs Robin, your razor needs moisturizer. They're the dynamic duo that will keep your face looking and feeling its best. Check out this face <a href="https://www.harrys.com/en/us/products/daily-face-lotion">moisturizer</a> from HARRY'S!</ul>
             </div>
-            <div className='image-column'>
-              
-
-            </div>
           </div>
+          <div>
+              <img src="Blade Razor.jpeg" className='freshface-right-img'></img>
+            </div>
           </div>
         
       </div>
